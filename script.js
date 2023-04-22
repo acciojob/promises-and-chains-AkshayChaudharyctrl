0f31b3e218ae627b559b43dcfb31a5cd4f97810e
AkshayChaudharyctrl <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-04-22 21:34:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-//your JS code here. If required.
-/your JS code here. If required.
-var nameElement = document.querySelector("#name");
-var ageElement = document.querySelector("#age");
-var btnElement = document.querySelector("#btn");
-
-var promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    var age = ageElement.value;
-    var name = nameElement.value;
-   var age = ageElement.value;
-    var name = nameElement.value;
-   if (!(age && name)) {
-     alert("Please enter valid details");
-   }
-    return;
-    if (age < 18) {
-      reject(`Oh sorry ${name}. You aren't old enough`);
-    } else {
-      resolve(`Welcome, ${name}. You can vote.`);
+let formbutton = document.getElementById("btn");
+function promiseApi1(name , age)
+{
+ return new Promise((resolve , reject) => {
+    if(age > 18)
+    {
+        setTimeout(() => {
+            resolve(
+                alert(`welcome , ${name}, You can vote`)
+                // console.log(`You can vote , ${name}`)
+            )
+        } , 4000)
     }
-  }, 4000);
-});
-
-
-function callPromise() {
-    .then((data) => {
-      alert(`Welcome, ${data.name}. You can vote.`);
-    })
-    .catch((error) => {
-      alert(`Oh sorry ${error.name}. You aren't old enough.`);
-    });
+    else
+    {
+        alert(`Oh sorry ${name}. You aren't old enough.`)
+    }
+ })
 }
-function handleSubmit(e) {
-    e.preventDefault();
-  callPromise().then(alert).catch(alert);
-}
-
-btnElement.addEventListener("click", handleSubmit);
+formbutton.addEventListener("click" , (event) => {
+    event.preventDefault();
+    let nameinput = document.getElementById("name").value;
+    let ageinput = document.getElementById("age").value;
+    if (nameinput == "" && ageinput == "") {
+        alert("It should not be empty")
+    }
+    else
+    {
+        promiseApi1(nameinput , ageinput);
+    }
+})
